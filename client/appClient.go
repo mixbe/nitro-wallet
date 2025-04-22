6356729154b7287c23a51d3e5985c73162a88a00
AuroraHare <aurorahare@gmail.com>
2025-04-22 10:27:54 +0800

    🐛 fix a bug

--- client/appClient.go
+++ client/appClient.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -298,7 +299,7 @@ func getIAMToken() iamCredentialResponse {
  */
 func getIAMTokenV2() iamCredentialResponse {
 	var token iamCredentialResponse
-	
+
 	// Step 1: Get a session token
 	tokenTTL := "60" // Time to live in seconds
 	tokenReq, err := http.NewRequest("PUT", "http://169.254.169.254/latest/api/token", nil)
@@ -306,39 +307,39 @@ func getIAMTokenV2() iamCredentialResponse {
 		log.Fatalf("Error creating token request: %v", err)
 	}
 	tokenReq.Header.Set("X-aws-ec2-metadata-token-ttl-seconds", tokenTTL)
-	
+
 	tokenClient := &http.Client{Timeout: 2 * time.Second}
 	tokenResp, err := tokenClient.Do(tokenReq)
 	if err != nil {
 		log.Fatalf("Error getting token: %v", err)
 	}
 	defer tokenResp.Body.Close()
-	
+
 	tokenBytes, err := io.ReadAll(tokenResp.Body)
 	if err != nil {
 		log.Fatalf("Error reading token response: %v", err)
 	}
 	sessionToken := string(tokenBytes)
-	
+
 	// Step 2: Use the token to get the instance profile name
 	profileReq, err := http.NewRequest("GET", "http://169.254.169.254/latest/meta-data/iam/security-credentials/", nil)
 	if err != nil {
 		log.Fatalf("Error creating profile request: %v", err)
 	}
 	profileReq.Header.Set("X-aws-ec2-metadata-token", sessionToken)
-	
+
 	profileResp, err := http.DefaultClient.Do(profileReq)
 	if err != nil {
 		log.Fatalf("Error getting instance profile: %v", err)
 	}
 	defer profileResp.Body.Close()
-	
+
 	profileBytes, err := io.ReadAll(profileResp.Body)
 	if err != nil {
 		log.Fatalf("Error reading profile response: %v", err)
 	}
 	instanceProfileName := string(profileBytes)
-	
+
 	// Step 3: Get the credentials using the token
 	credentialsURL := fmt.Sprintf("http://169.254.169.254/latest/meta-data/iam/security-credentials/%s", instanceProfileName)
 	credentialsReq, err := http.NewRequest("GET", credentialsURL, nil)
@@ -346,60 +347,60 @@ func getIAMTokenV2() iamCredentialResponse {
 		log.Fatalf("Error creating credentials request: %v", err)
 	}
 	credentialsReq.Header.Set("X-aws-ec2-metadata-token", sessionToken)
-	
+
 	credentialsResp, err := http.DefaultClient.Do(credentialsReq)
 	if err != nil {
 		log.Fatalf("Error getting credentials: %v", err)
 	}
 	defer credentialsResp.Body.Close()
-	
+
 	credentialsBytes, err := io.ReadAll(credentialsResp.Body)
 	if err != nil {
 		log.Fatalf("Error reading credentials response: %v", err)
 	}
-	
+
 	var result iamCredentialToken
 	err = json.Unmarshal(credentialsBytes, &result)
 	if err != nil {
 		log.Fatalf("Error unmarshaling credentials: %v", err)
 	}
-	
+
 	fmt.Println("Retrieved credentials using IMDSv2")
-	
+
 	token.aws_access_key_id = result.AccessKeyId
 	token.aws_secret_access_key = result.SecretAccessKey
 	token.aws_session_token = result.Token
-	
+
 	return token
 }
 
-/**	
+/**
 * 创建 EVM 账户
 * go run appClient.go account1 EVM
 *
 * 创建 Solana 账户
 * go run appClient.go solana_account1 SOLANA
-*/
+ */
 func main() {
 	region := "ap-northeast-1"
 	keyId := "fb852884-e2f0-4a06-9bfe-edd5d0792b46"
 	tableName := "AccountTable"
-	
+
 	// 默认账户名和链类型
 	walletAccountName := "account1"
 	chainType := "EVM"
-	
+
 	// 从命令行参数获取账户名和链类型
 	if len(os.Args) > 1 {
 		walletAccountName = os.Args[1]
 		fmt.Println("使用命令行指定的账户名:", walletAccountName)
 	}
-	
+
 	if len(os.Args) > 2 {
 		chainType = strings.ToUpper(os.Args[2])
 		fmt.Println("使用命令行指定的链类型:", chainType)
 	}
-	
+
 	// 检查链类型是否有效
 	if chainType != "EVM" && chainType != "SOLANA" {
 		fmt.Println("错误: 链类型必须是 EVM 或 SOLANA")
@@ -408,7 +409,7 @@ func main() {
 		fmt.Println("示例: go run appClient.go solana_account1 SOLANA")
 		return
 	}
-	
+
 	// check dynamodb AccountTable exist or not, create it if not exists
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(region)},
@@ -481,7 +482,7 @@ func main() {
 			},
 			TableName: aws.String(tableName),
 		}
-		
+
 		result, err := svc.CreateTable(create_input)
 		if err != nil {
 			fmt.Println(err.Error())
@@ -494,31 +495,31 @@ func main() {
 	// 设置默认链类型
 	defaultChainType := "EVM"
 	client := accountClient{region, tableName, keyId, 16, 5000, defaultChainType}
-	
+
 	// 生成账户
 	fmt.Printf("正在为账户 %s 生成 %s 类型的地址...\n", walletAccountName, chainType)
 	client.generateAccount(walletAccountName, chainType)
 	fmt.Println("账户生成完成!")
-	
+
 	// 如果需要测试签名，取消下面的注释
 	/*
-	//测试签名
-	transaction := map[string]interface{}{
-		"value":    1000000000,
-		"to":       "0xF0109fC8DF283027b6285cc889F5aA624EaC1F55",
-		"nonce":    0,
-		"chainId":  4,
-		"gas":      100000,
-		"gasPrice": 234567897654321,
-	}
+		//测试签名
+		transaction := map[string]interface{}{
+			"value":    1000000000,
+			"to":       "0xF0109fC8DF283027b6285cc889F5aA624EaC1F55",
+			"nonce":    0,
+			"chainId":  4,
+			"gas":      100000,
+			"gasPrice": 234567897654321,
+		}
 
-	b := new(bytes.Buffer)
-	for key, value := range transaction {
-		fmt.Fprintf(b, "%s=\"%s\"\n", key, value)
-	}
+		b := new(bytes.Buffer)
+		for key, value := range transaction {
+			fmt.Fprintf(b, "%s=\"%s\"\n", key, value)
+		}
 
-	// 签名
-	signedValue := client.sign(keyId, walletAccountName, chainType, b.String())
-	fmt.Println("签名结果:", signedValue)
+		// 签名
+		signedValue := client.sign(keyId, walletAccountName, chainType, b.String())
+		fmt.Println("签名结果:", signedValue)
 	*/
 }
